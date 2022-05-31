ed022d219655c3eccc0fabae934907907916afc7
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2022-05-31 12:04:29 +0500

    tic tac toe

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,21 +1,13 @@
 import React from "react";
 
-const style ={
-    background:'rgb(33, 207, 170)',
-    border: '1px solid white',
-    fontSize :'30px',
-    fontWeight: '800',
-    cursor: 'pointer',
-    outline: 'none' ,
-    color: "white"
-};
+
+
 
 const Square = ({value, onClick}) => (
-    <button style={style} onClick={onClick}>
+    <button  onClick={onClick}>
         {value}
     </button>
 
 );
-
 
 export default Square;
